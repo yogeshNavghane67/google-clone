e3559109b646a630d06352a21822eb3b4ece6d1f
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-04 23:43:40 +0530

    pagination component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function WebSearchPage({searchParams}) {
+  const startIndex = searchParams.start || '1';
   const response = await fetch(
-    ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
   if(!response.ok) throw new Error('Something went Wrong');
   const data = await response.json();
