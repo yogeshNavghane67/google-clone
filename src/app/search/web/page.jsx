affb2b937124379a72d12678cf5b4672a6804a2a
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-05 00:05:45 +0530

    loading effect for web and image pages

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 
 export default async function WebSearchPage({searchParams}) {
   const startIndex = searchParams.start || '1';
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
