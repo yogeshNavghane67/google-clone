539c4865c3b8a3b51206f9dfd8759ff2126b48c4
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-07-05 21:28:27 +0530

    api link fix 2

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -6,7 +6,7 @@ export default async function ImageSearchPage({searchParams}) {
   const startIndex = searchParams.start || '1';
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
-    ` https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${startIndex}'}&searchType=image&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
   if(!response.ok) throw new Error('Something went Wrong');
   const data = await response.json();
